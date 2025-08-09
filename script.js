7815a1efde9bb6ab59a09827c3845613905a7130
Muhammad Mehboob <muhammadmehboobz7869@gmail.com>
2025-08-09 01:26:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  const navToggle = document.getElementById('nav-toggle');
+  const navMenu = document.getElementById('nav-menu');
+
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show-menu');
+  });
+
+  // Optional: Close menu when a link is clicked
+  const navLinks = document.querySelectorAll('.nav__link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu');
+    });
+  });
+
